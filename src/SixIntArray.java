624b8bd27cf41852fcfe066a1e61253985dadfe8
Cole DeBoer <Cole@Thes-MacBook-Pro-2.local>
2019-05-10 16:54:22 -0500

    First commit

---
+++ src/SixIntArray.java
@@ -0,0 +1,31 @@
+
+public class SixIntArray {
+	public static void main(String[]args)
+	{
+		int favNums[], i = 0;
+		favNums = new int[6];
+		favNums[0] = 7;
+		favNums[1] = 14;
+		favNums[2] = 69;
+		favNums[3] = 420;
+		favNums[4] = 999;
+		favNums[5] = 99;
+		
+		for(i = 0; i < favNums.length; i++) {
+			System.out.println("Element " + (i + 1) + ": " + favNums[i]);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
